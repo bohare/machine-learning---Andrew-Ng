429f325887ca85f43847cb08958b16a05b6af761
dovahkiin2 <dovahkiin2@users.noreply.github.com>
2016-05-13 10:03:18 +0530

    Exercise 2 - Logistic Regression

--- plotData.m
+++ plotData.m
@@ -1,29 +1,35 @@
-function plotData(x, y)
-%PLOTDATA Plots the data points x and y into a new figure 
-%   PLOTDATA(x,y) plots the data points and gives the figure axes labels of
-%   population and profit.
+function plotData(X, y)
+%PLOTDATA Plots the data points X and y into a new figure 
+%   PLOTDATA(x,y) plots the data points with + for the positive examples
+%   and o for the negative examples. X is assumed to be a Mx2 matrix.
+
+% Create New Figure
+figure; hold on;
 
 % ====================== YOUR CODE HERE ======================
-% Instructions: Plot the training data into a figure using the 
-%               "figure" and "plot" commands. Set the axes labels using
-%               the "xlabel" and "ylabel" commands. Assume the 
-%               population and revenue data have been passed in
-%               as the x and y arguments of this function.
+% Instructions: Plot the positive and negative examples on a
+%               2D plot, using the option 'k+' for the positive
+%               examples and 'ko' for the negative examples.
 %
-% Hint: You can use the 'rx' option with plot to have the markers
-%       appear as red crosses. Furthermore, you can make the
-%       markers larger by using plot(..., 'rx', 'MarkerSize', 10);
-
-figure; % open a new figure window
-plot(x, y, 'rx', 'Markersize', 10)  %plot the data
-ylabel('Profit in $10,000s')    %Set the y-axis label
-xlabel('Population of the city in 10,000s') %Set the x-axis label
-
+% Find Indices of Positive and Negative Examples
+pos = find(y==1); neg = find(y == 0);
+% Plot Examples
+plot(X(pos, 1), X(pos, 2), 'k+','LineWidth', 2, ...
+'MarkerSize', 7);
+plot(X(neg, 1), X(neg, 2), 'ko', 'MarkerFaceColor', 'y', ...
+'MarkerSize', 7);
 
 
 
 
 
-% ============================================================
+
+
+
+% =========================================================================
+
+
+
+hold off;
 
 end
